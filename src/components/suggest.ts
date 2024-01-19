746f2274b1243a140c545c4ab2a9894024a7a99b
Leonardo Alvaro <arl880895@protonmail.com>
2024-01-18 23:29:05 -0600

    REFACTOR(suggest.ts): Utility functions moved to utils.ts

--- src/components/suggest.ts
+++ src/components/suggest.ts
@@ -10,6 +10,11 @@ import BundlePlugin from "main";
 import BundleComponent from "types";
 
 import {
+  wrapAround,
+  getTFilesFromFolder
+} from "utils";
+
+import {
   DateFormat,
   getMatchedDate,
 } from "components/time";
@@ -311,7 +316,7 @@ abstract class CustomSuggest<T> {
     // On Input Change
     this.inputEl.addEventListener("input", async () => {
       await this.setResults(this.inputEl.value);
-      console.log("----------------");
+      // console.log("----------------");
       
     })
 
@@ -734,52 +739,4 @@ function getHeadings(fileText: string): Heading[] {
   return headings;
 }
 
-
-////////////////////////////////////////
-// UTILITY FUNCTIONS
-////////////////////////////////////////
-
-const wrapAround = (value: number, size: number): number => {
-    return ((value % size) + size) % size;
-};
-
-
-function resolveTFile(app: App, fileStr: string): TFile {
-  const file = app.vault.getAbstractFileByPath(fileStr);
-  if (!file) {
-    throw new Error(`File "${fileStr}" does not exist.`);
-  }
-  if (!(file instanceof TFile)) {
-    throw new Error(`"${fileStr}" is a folder, not a file.`);
-  }
-  return file;
-}
-
-
-function resolveTFolder(app: App, folderStr: string): TFolder {
-  const folder = app.vault.getAbstractFileByPath(folderStr);
-  if (!folder) {
-    throw new Error(`Folder "${folderStr}" does not exist.`);
-  }
-  if (!(folder instanceof TFolder)) {
-    throw new Error(`"${folderStr}" is a file, not a folder.`);
-  }
-  return folder;
-}
-
-
-function getTFilesFromFolder(app: App, folderStr: string): Array<TFile> {
-  const folder = resolveTFolder(app, folderStr);
-
-  const files: Array<TFile> = [];
-  Vault.recurseChildren(folder, (file: TAbstractFile) => {
-    if (file instanceof TFile) {
-      files.push(file);
-    }
-  });
-
-  files.sort((a, b) => a.basename.localeCompare(b.basename));
-
-  return files;
-}
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import {
-  Notice, TFile,
+  App, Vault, Notice,
+  TAbstractFile, TFolder, TFile,
   Editor, MarkdownView,
   CachedMetadata, HeadingCache,
   EditorRange, EditorRangeOrCaret,
@@ -125,3 +126,52 @@ export function handleCursorMovement(
   editor.transaction({selection});
 }
 
+
+
+////////////////////////////////////////
+// OTHER UTILITY FUNCTIONS
+////////////////////////////////////////
+
+export function wrapAround(value: number, size: number): number {
+    return ((value % size) + size) % size;
+}
+
+
+export function resolveTFile(app: App, fileStr: string): TFile {
+  const file = app.vault.getAbstractFileByPath(fileStr);
+  if (!file) {
+    throw new Error(`File "${fileStr}" does not exist.`);
+  }
+  if (!(file instanceof TFile)) {
+    throw new Error(`"${fileStr}" is a folder, not a file.`);
+  }
+  return file;
+}
+
+
+export function resolveTFolder(app: App, folderStr: string): TFolder {
+  const folder = app.vault.getAbstractFileByPath(folderStr);
+  if (!folder) {
+    throw new Error(`Folder "${folderStr}" does not exist.`);
+  }
+  if (!(folder instanceof TFolder)) {
+    throw new Error(`"${folderStr}" is a file, not a folder.`);
+  }
+  return folder;
+}
+
+
+export function getTFilesFromFolder(app: App, folderStr: string): Array<TFile> {
+  const folder = resolveTFolder(app, folderStr);
+
+  const files: Array<TFile> = [];
+  Vault.recurseChildren(folder, (file: TAbstractFile) => {
+    if (file instanceof TFile) {
+      files.push(file);
+    }
+  });
+
+  files.sort((a, b) => a.basename.localeCompare(b.basename));
+
+  return files;
+}
