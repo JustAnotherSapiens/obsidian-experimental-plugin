418981731bf4a234bd880e01124f5ecc96f9d444
Leonardo Alvaro <arl880895@protonmail.com>
2024-05-11 18:07:18 -0600

    ADD(mdEditings): smartSurround for various pairs added.

---
+++ project-bump.mjs
@@ -0,0 +1,38 @@
+
+// USAGE:
+// - Update name, version, and description in package.json
+// - Update minAppVersion in manifest.json
+// - Run `npm run version`
+// - Commit changes
+
+// EFFECTS:
+// - manifest.json fields updated: name, version, description
+// - versions.json gets updated with new version and minAppVersion
+
+
+import { readFileSync, writeFileSync } from "fs";
+
+
+// Display package name and version from the process environment
+const packageName = process.env.npm_package_name;
+const packageVersion = process.env.npm_package_version;
+console.log(`Bumping version of "${packageName}" to ${packageVersion}`);
+
+// Display package name and version from package.json
+const packageJson = JSON.parse(readFileSync(process.env.npm_package_json, "utf8"));
+const { name, version, description } = packageJson;
+console.log(`${name} v${version} - ${description}`);
+
+
+// Update manifest.json with package.json values
+const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+const { minAppVersion } = manifest;
+manifest.name = packageJson.name;
+manifest.version = packageJson.version;
+manifest.description = packageJson.description;
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+
+// Update versions.json with new version and minAppVersion
+const versions = JSON.parse(readFileSync("versions.json", "utf8"));
+versions[packageJson.version] = minAppVersion;
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
--- src/components/others/mdEditing.ts
+++ src/components/others/mdEditing.ts
@@ -1,10 +1,79 @@
 import {
   App,
   Editor,
+  EditorRange,
+  IconName,
 } from "obsidian";
 
 import BundlePlugin from "main";
 import { BundleComponent } from "main";
+
+
+
+type SurroundPair = {
+  start: string;
+  end: string;
+  name?: string;
+  icon?: IconName;
+};
+
+
+const defaultSurroundPairs: SurroundPair[] = [
+  {
+    name: "Bold",
+    start: "**",
+    end: "**",
+    icon: "bold",
+  },
+  {
+    name: "Italic",
+    start: "*",
+    end: "*",
+    icon: "italic",
+  },
+  {
+    name: "Highlight",
+    start: "==",
+    end: "==",
+    icon: "highlighter",
+  },
+  {
+    name: "Strikethrough",
+    start: "~~",
+    end: "~~",
+    icon: "strikethrough",
+  },
+  {
+    name: "Code",
+    start: "`",
+    end: "`",
+    icon: "code",
+  },
+  {
+    name: "Comment",
+    start: "%%",
+    end: "%%",
+    icon: "message-square-text",
+  },
+  {
+    name: "Inline Math",
+    start: "$",
+    end: "$",
+    icon: "square-sigma",
+  },
+  {
+    name: "Block Math",
+    start: "$$",
+    end: "$$",
+    icon: "sigma",
+  },
+  {
+    name: "Underline",
+    start: "<u>",
+    end: "</u>",
+    icon: "underline",
+  },
+];
 
 
 
@@ -34,17 +103,14 @@ export default class MdEditingComponent implements BundleComponent {
   addCommands(): void {
     const plugin = this.parent;
 
-		// Behavior-tailored strikethrough
-		plugin.addCommand({
-			id: "smart-strikethrough",
-			name: "Smart strikethrough",
-			icon: "strikethrough",
-			mobileOnly: false,
-			repeatable: false,
-			editorCallback: (editor: Editor) => {
-				smartStrikethrough(editor);
-			}
-		});
+    defaultSurroundPairs.forEach(pair => {
+      plugin.addCommand({
+        id: `smart-${pair.name!.toLowerCase().replace(" ", "-")}`,
+        name: `Smart ${pair.name!}`,
+        icon: pair.icon,
+        editorCallback: (editor) => smartSurround(editor, pair),
+      });
+    });
 
   }
 
@@ -56,16 +122,13 @@ export default class MdEditingComponent implements BundleComponent {
 
 
 
-export function smartStrikethrough(editor: Editor, linewise: boolean = true) {
-
-  // Full-line selection range (if any), cursor line range otherwise
-  const editRange = {
+export function smartSurround(
+  editor: Editor, pair: SurroundPair, linewise: boolean = true
+) {
+  const editRange: EditorRange = {
     from: {line: editor.getCursor("from").line, ch: 0},
-    to: {
-      line: editor.getCursor("to").line,
-      ch: editor.getLine(editor.getCursor("to").line).length
-    },
-  }
+    to: {line: editor.getCursor("to").line, ch: Infinity},
+  };
 
   // Get text to edit
   const isSingleLine = editRange.from.line === editRange.to.line;
@@ -77,30 +140,35 @@ export function smartStrikethrough(editor: Editor, linewise: boolean = true) {
   let changes = [];
   let endCursorPos = editRange.from;
 
+  const getPairRegex = (flags: string = "") => {
+    return new RegExp(`(?:${pair.start}\|${pair.end})`, flags);
+  };
+
   // Unstrike
-  if (editText.match(/~~/)) {
-    changes.push({text: editText.replace(/~~/g, ""), ...editRange});
+  // TODO: Add a stack-based algorithm to avoid removing only one of the pair
+  if (editText.match(getPairRegex())) {
+    changes.push({text: editText.replace(getPairRegex('g'), ""), ...editRange});
   }
 
   // Strike single line selection
   else if (oneLineSelection || !linewise) {
     changes.push({
-      text: editText.replace(/^(\s*)(.*?)(\s*)$/, "$1~~$2~~$3"),
+      text: editText.replace(/^(\s*)(.*?)(\s*)$/, `$1${pair.start}$2${pair.end}$3`),
       from: editor.getCursor("from"),
       to: editor.getCursor("to"),
     });
     endCursorPos = editor.getCursor();
-    endCursorPos.ch += 4; // To account for the added tildes (~~~~)
+    endCursorPos.ch += (pair.start.length + pair.end.length);
   }
 
   // Smart strike
   else {
-    let regex = /^((?:\s*>{1,6} )?\s*(?:(?:[-*+]|\d{1,6}\.) )?\s*)(.*?)(\s*)$/;
+    let smartRegex = /^((?:\s*>{1,6} )?\s*(?:(?:[-*+]|\d{1,6}\.) )?\s*)(.*?)(\s*)$/;
     let lines = editText.split('\n');
     for (let i = 0; i < lines.length; i++) {
       if (lines[i].trim() === '') continue;
       else {
-        lines[i] = lines[i].replace(regex, "$1~~$2~~$3");
+        lines[i] = lines[i].replace(smartRegex, `$1${pair.start}$2${pair.end}$3`);
       }
     }
     changes.push({text: lines.join('\n'), ...editRange});
